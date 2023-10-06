23b1ab2ec71ab3e7632a72ac6012bd47c41d8b16
Prajeen Govardhanam <prajeenrg@gmail.com>
2023-10-06 21:54:30 +0530

    refactor: initial changes to download ant tool

--- README.md
+++ README.md
@@ -15,10 +15,9 @@
 
 # Dependencies
 
-**TODO: adapt this section**
-
 - `bash`, `curl`, `tar`: generic POSIX utilities.
-- `SOME_ENV_VAR`: set this environment variable in your shell config to load the correct version of tool x.
+- `java` - Java runtime is needed, preferably Java 8 or latest.
+- `JAVA_HOME`: set this environment variable to correctly run the ant tool.
 
 # Install
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for ant.
 GH_REPO="https://github.com/apache/ant"
+DIST_URL="https://archive.apache.org/dist/ant/binaries"
 TOOL_NAME="ant"
 TOOL_TEST="ant --help"
 
@@ -14,26 +14,13 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if ant is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if ant has other means of determining installable versions.
-	list_github_tags
+	curl -sL "$DIST_URL" | grep -oe ">apache-ant-.*-bin.tar.gz<" | cut -d '-' -f3
 }
 
 download_release() {
@@ -41,8 +28,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for ant
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$DIST_URL/apache-ant-$version-bin.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +47,6 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert ant executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
